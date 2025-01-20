15c3e5721fdda6a4862dda52a827e83a12b89235
Anny <101419541+annykh@users.noreply.github.com>
2025-01-20 18:32:47 +0400

    Create Summary Session 1.sql

---
+++ Summary Session 1.sql
@@ -0,0 +1,15 @@
+-- Создание базы данных 
+-- create database название_базы_данных;
+create database gt081024;
+
+create database gt081024;
+-- Error Code: 1007. Can't create database 'gt081024'; database exists	0.000 sec
+
+-- Активация базы данных 
+use gt081024;
+
+-- Первая молния - запуск выделенного кода или всего скрипта
+-- ctrl+shift+enter
+
+-- Вторая молния - запуск запроса, где стоит курсор.
+-- ctrl+enter
