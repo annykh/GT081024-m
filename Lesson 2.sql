10ce3a05865d1db19fd722a7f810a9e28edc2f00
Anny <101419541+annykh@users.noreply.github.com>
2025-01-22 16:11:16 +0400

    Update Lesson 2.sql

--- Lesson 2.sql
+++ Lesson 2.sql
@@ -111,3 +111,92 @@ from products
 where price between 100 and 150;
 
 -- Вывести продукты, где цена больше 100 и кол/во меньше 10.
+select *
+from products
+where price > 100 and quantity < 10;
+
+-- is null
+-- is not null
+
+-- Вывести продукт, где цена не указана
+select * 
+from products
+where price is null;
+
+-- create table название_таблицы(
+-- 	название_поля1 тип_данных [ограничения],
+--  назнавие_поля2 тип_данных [ограничения],
+--     ...
+-- );
+
+-- Ограничения/атрибуты таблиц 
+
+-- unique - значения в поле должны быть уникальными 
+-- not null - значения в поле не могут быть незаполненными (null)
+-- primary key - not null + unique - в таблцие можно использовать только один раз
+-- check(условие) - значение поле сответствует какому-либо условию 
+-- default значение - у поля будет значение по умолчанию 
+-- auto_increment - позволяет создавать уникальный номер автоматически, когда новая 
+-- запись вставляется в таблицу. Часто это поле основного ключа, которое мы хотели бы создать 
+-- автоматически каждый раз, когда будет вставлена новая запись.
+
+-- Создать таблицу students
+-- id целое число, уникальное значение, не null, автозаполнение 
+-- firstname varchar not null
+-- lastname varchar not null
+-- class integer от 1 до 10 (вкл)
+-- subject_name varchar not null
+-- mark integer от 0 до 5 (вкл)
+-- school_no integer по умолч. 0
+-- checked char(1) значение Y либо N
+
+create table students(
+	id int primary key auto_increment,
+    firstname varchar(100) not null,
+    lastname varchar(100) not null,
+    class int check(class between 1 and 10),
+    subject_name varchar(100) not null,
+    mark int check(mark between 0 and 5),
+    school_no int default 0,
+    checked char(1) check(checked in ('Y', 'N'))
+);
+
+select * from students;
+
+-- Заполнение с ошибками 
+insert into students
+values ('John', 'Smith', 5, 'Math', 4, 120, 'N');
+-- Error Code: 1136. Column count doesn't match value count at row 1	0.000 sec
+
+-- Решение
+insert into students (firstname, lastname, class, subject_name, mark, school_no, checked)
+values ('John', 'Smith', 5, 'Math', 4, 120, 'N');
+
+insert into students (lastname, class, subject_name, mark, school_no, checked)
+values ('Smith', 5, 'Math', 4, 120, 'N');
+-- Error Code: 1364. Field 'firstname' doesn't have a default value	0.000 sec
+
+insert into students (firstname, lastname, class, subject_name, mark, school_no, checked)
+values ('John', 'Smith', 15, 'Math', 4, 120, 'N');
+-- Error Code: 3819. Check constraint 'students_chk_1' is violated.	0.016 sec
+
+insert into students (firstname, lastname, class, subject_name, mark, school_no, checked)
+values ('John', 'Smith', 10, 'Math', 10, 120, 'N');
+-- Error Code: 3819. Check constraint 'students_chk_2' is violated.	0.000 sec
+
+insert into students (firstname, lastname, class, subject_name, mark, checked)
+values ('John', 'Smith', 10, 'Math', 5, 'N');
+
+insert into students (firstname, lastname, class, subject_name, mark, school_no, checked)
+values ('John', 'Smith', 10, 'Math', 4, 120, 'L');
+-- Error Code: 3819. Check constraint 'students_chk_3' is violated.	0.000 sec
+
+insert into students (firstname, lastname, class, subject_name, mark, school_no, checked)
+values ('John', 'Smith', 10, 'Math', 4, 120, 'NN');
+-- Error Code: 1406. Data too long for column 'checked' at row 1	0.016 sec
+
+insert into students (firstname, lastname, class, subject_name, mark, school_no, checked)
+values ('John', 'Smith', 'ten', 'Math', 4, 120, 'N');
+-- Error Code: 1366. Incorrect integer value: 'ten' for column 'class' at row 1	0.000 sec
+
+select * from students;
