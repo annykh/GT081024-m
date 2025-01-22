60876b31a8e7e817b63bb081a3cc8a532117c46f
Anny <101419541+annykh@users.noreply.github.com>
2025-01-22 16:14:48 +0400

    Create hw_lesson_2.sql

---
+++ hw_lesson_2.sql
@@ -0,0 +1,10 @@
+1. Создать базу данных homeworks
+2.  В этой базе создать таблицу staff с полями:
+ • id – integer - уникальное значение, не пустое, с автозаполнением 
+ • firstname - varchar - не пустое
+ • lastname - varchar - не пустое
+ • position - varchar
+ • age - integer - от 0 до 110(вкл.)
+ • has_child – char только один символ, по умолчанию 'N' 
+ • username - varchar - уникальный
+3. Добавить 5 строк.
