939e5cd454bd0ac3fbb855bf118cb0b7f5685ffc
Anny <101419541+annykh@users.noreply.github.com>
2025-05-14 16:31:17 +0400

    Update Summary Session 14.js

--- Summary Session 14.js
+++ Summary Session 14.js
@@ -1,6 +1,8 @@
 // TASKS
 
 // 1. В базе данных tasks создать пустую коллекцию employees.
+use tasks
+db.createCollection('employees')
 
 // 2. Заполнить коллекцию свойствами id, firstname, lastname, age, position, salary, skills.
     // 1. Inga Petrova, 27, Barista, 1500, ['preparing drinks', 'cleaning equipment'] 
@@ -9,7 +11,56 @@
     // 4. Marina Sidorova, 22, Hostess, 1700, ['greeting guests', 'seating guests', 'answering phone calls']
     // 5. Olga Ivanova, 43, Sommelier, 2500, ['curating a wine list', 'creating wine pairings']
 
+db.employees.insertMany([
+    {
+        id: 1,
+        firstname: 'Inga',
+        lastname: 'Petrova',
+        age: 27,
+        position: 'Barista',
+        salary: 1500,
+        skills: ['preparing drinks', 'cleaning equipment']
+    },
+    {
+        id: 2,
+        firstname: 'Boris',
+        lastname: 'Orlov',
+        age: 36,
+        position: 'Server',
+        salary: 2400,
+        skills: ['taking orders', 'suggesting meals', 'taking payments']
+    },
+    {
+        id: 3,
+        firstname: 'Ivan',
+        lastname: 'Demidov',
+        age: 32,
+        position: 'Chef',
+        salary: 3200,
+        skills: ['preparing food', 'baking bread']
+    },
+    {
+        id: 4,
+        firstname: 'Marina',
+        lastname: 'Sidorova',
+        age: 22,
+        position: 'Hostess',
+        salary: 1700,
+        skills: ['greeting guests', 'seating guests', 'answering phone calls']
+    },
+    {
+        id: 5,
+        firstname: 'Olga',
+        lastname: 'Ivanova',
+        age: 43,
+        position: 'Sommelier',
+        salary: 2500,
+        skills: ['curating a wine list', 'creating wine pairings']
+    }
+])
+
 // 3. Вывести коллекцию employees.
+db.employees.find()
 
 // 4. Найти и исправить ошибку в коде:
 //  db.employee.insertOne({id: 1,
@@ -19,6 +70,13 @@
 //           salary: 1500,
 //           skills: ['coding', 'team-working']) 
 
+//  db.employee.insertOne({id: 1,
+//           firstname: 'Lida',
+//           lastname: 'Petrova',
+//           age: 27,
+//           salary: 1500,
+//           skills: ['coding', 'team-working']}) 
+
 // 5. Найти и исправить ошибку в коде:
 //  db.employee.insertMany([{id: 1,
 //           firstname: 'Lida',
@@ -26,6 +84,15 @@
 //           age: 27,
 //           salary: 1500,
 //           skills: ['coding', 'team-working']]})
+
+//  db.employee.insertMany([
+//           {id: 1,
+//           firstname: 'Lida',
+//           lastname: 'Petrova',
+//           age: 27,
+//           salary: 1500,
+//           skills: ['coding', 'team-working']
+// }])
 
 // 6. Найти и исправить ошибки в коде:
 // db.students.insertMany([
@@ -51,7 +118,33 @@
 // 	}
 // ))
 
+
+db.students.insertMany([
+	{
+	     id: 1,
+	     name: 'Amy',
+	     avg_mark: 5		
+	},
+	{
+	     id: 2,
+	     name: 'Sam',
+	     avg_mark: 3		
+	},
+	{
+	     id: 3,
+	     name: 'Ben',
+	     avg_mark: 4.7		
+	},
+	{
+	     id: 4,
+	     name: 'Max',
+	     avg_mark: 2		
+	}
+])
+
+
 // 7. Создать бд shop
+use shop
 
 // 8. Создать коллекцию products1 и заполнить документами (6 штук) со следующими свойствами (id, title, price, count). Используйте следующие данные
   // 1 Велосипед 45000 12
@@ -61,6 +154,47 @@
   // 5 Скейт 19000 4
   // 6 Сноуборд 33000 17
 
+db.products1.insertMany([
+    {
+        id: 1, 
+        title: 'Велосипед',
+        price: 45000, 
+        count: 12
+    },
+    {
+        id: 2, 
+        title: 'Самокат',
+        price: 1500, 
+        count: 10
+    },
+    {
+        id: 3, 
+        title: 'Ролики',
+        price: 2000, 
+        count: 20
+    },
+    {
+        id: 4, 
+        title: 'Лыжи',
+        price: 22000, 
+        count: 15
+    },
+    {
+        id: 5, 
+        title: 'Скейт',
+        price: 19000, 
+        count: 4
+    },
+    {
+        id: 6, 
+        title: 'Сноуборд',
+        price: 33000, 
+        count: 17
+    }    
+])
+
 // 9. Удалить коллекцию products1
+db.products1.drop()
 
 // 10. Удалить бд shop
+db.dropDatabase()
